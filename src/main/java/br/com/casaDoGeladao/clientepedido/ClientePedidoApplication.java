788fbd8fc3ca67967442ca93fbdbab22d13d06c3
unknown <santosvinepolvora@gmail.com>
2022-12-11 22:09:59 -0300

    implementa modificacao cliente pedido

--- src/main/java/br/com/casaDoGeladao/clientepedido/ClientePedidoApplication.java
+++ src/main/java/br/com/casaDoGeladao/clientepedido/ClientePedidoApplication.java
@@ -2,9 +2,17 @@ package br.com.casaDoGeladao.clientepedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePedidoApplication {
+	
+	public String getHomeTeste() {
+		return "Cliente Pedido - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePedidoApplication.class, args);
