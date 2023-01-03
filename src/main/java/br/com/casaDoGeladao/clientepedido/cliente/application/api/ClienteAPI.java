5fa9fb3025dc709a28db8b38190f9b536e4310d9
unknown <santosvinepolvora@gmail.com>
2023-01-03 00:33:38 -0300

    modifica contrato da API

--- src/main/java/br/com/casaDoGeladao/clientepedido/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/casaDoGeladao/clientepedido/cliente/application/api/ClienteAPI.java
@@ -1,11 +1,16 @@
 package br.com.casaDoGeladao.clientepedido.cliente.application.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
-	
-	ClieteResponse postCliente(ClienteRequest clienteRequest);
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	ClieteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
 }
