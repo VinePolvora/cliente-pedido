c413bcf74e01ff9ddcae08ddb21c554109f05674
unknown <santosvinepolvora@gmail.com>
2023-01-06 04:51:23 -0300

    implementa getTodos

--- src/main/java/br/com/casaDoGeladao/clientepedido/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/casaDoGeladao/clientepedido/cliente/application/api/ClienteListResponse.java
@@ -2,9 +2,12 @@ package br.com.casaDoGeladao.clientepedido.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.casaDoGeladao.clientepedido.cliente.domain.Cliente;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
 	private UUID idCliente;
 	private String nomeCompleto;
@@ -13,8 +16,18 @@ public class ClienteListResponse {
 	private String celular;
 	
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		// TODO Auto-generated method stub
-		return null;
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
 	}
 
+	private ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.cpf = cliente.getCpf();
+		this.email = cliente.getEmail();
+		this.celular = cliente.getCelular();
+	}
+
+	
 }
