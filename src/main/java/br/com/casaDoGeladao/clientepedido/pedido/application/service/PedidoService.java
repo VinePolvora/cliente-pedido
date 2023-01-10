3cb46674b6f4195bc78b1db564a3dcf5545c2457
unknown <santosvinepolvora@gmail.com>
2023-01-10 05:56:28 -0300

    implementa post do Pedido

--- src/main/java/br/com/casaDoGeladao/clientepedido/pedido/application/service/PedidoService.java
+++ src/main/java/br/com/casaDoGeladao/clientepedido/pedido/application/service/PedidoService.java
@@ -1,11 +1,14 @@
 package br.com.casaDoGeladao.clientepedido.pedido.application.service;
 
+import java.util.List;
 import java.util.UUID;
 
+import br.com.casaDoGeladao.clientepedido.pedido.application.api.PedidoClienteListResponse;
 import br.com.casaDoGeladao.clientepedido.pedido.application.api.PedidoRequest;
 import br.com.casaDoGeladao.clientepedido.pedido.application.api.PedidoResponse;
 import jakarta.validation.Valid;
 
 public interface PedidoService {
 	PedidoResponse criaPedido(UUID idCliente, @Valid PedidoRequest pedidoRequest);
+	List<PedidoClienteListResponse> buscaPedidosDoClienteComId(UUID idCliente);
 }
