fa940a0b4972fd537f3e725cbeff4dc0fa544de7
unknown <santosvinepolvora@gmail.com>
2023-01-10 07:34:41 -0300

    implementa repository infra por busca pets por idcliente

--- src/main/java/br/com/casaDoGeladao/clientepedido/pedido/infra/PedidoSpringDataJPARepository.java
+++ src/main/java/br/com/casaDoGeladao/clientepedido/pedido/infra/PedidoSpringDataJPARepository.java
@@ -1,5 +1,6 @@
 package br.com.casaDoGeladao.clientepedido.pedido.infra;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.casaDoGeladao.clientepedido.pedido.domain.Pedido;
 
 public interface PedidoSpringDataJPARepository extends JpaRepository<Pedido, UUID> {
-
+	 List<Pedido> findByIdClientePedido(UUID idClientePedido); 
 }
