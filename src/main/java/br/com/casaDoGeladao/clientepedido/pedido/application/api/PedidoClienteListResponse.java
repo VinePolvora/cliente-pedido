1e02cddbfa046383743bcf3cf867140600cd7784
unknown <santosvinepolvora@gmail.com>
2023-01-10 08:17:40 -0300

    implementa conversao pedidoCliente listResponse

--- src/main/java/br/com/casaDoGeladao/clientepedido/pedido/application/api/PedidoClienteListResponse.java
+++ src/main/java/br/com/casaDoGeladao/clientepedido/pedido/application/api/PedidoClienteListResponse.java
@@ -1,11 +1,30 @@
 package br.com.casaDoGeladao.clientepedido.pedido.application.api;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.casaDoGeladao.clientepedido.pedido.domain.Pedido;
+import br.com.casaDoGeladao.clientepedido.pedido.domain.Sabor;
+import lombok.Getter;
 
+@Getter
 public class PedidoClienteListResponse {
+	private UUID idPedido;
+	private Sabor sabor;
+	private int quantidade;
+	
 	public static List<PedidoClienteListResponse> converte(List<Pedido> pedidosDoCliente) {
-		return null;
+		return pedidosDoCliente.stream()
+				.map(PedidoClienteListResponse::new)
+				.collect(Collectors.toList());	
 	}
+
+	public PedidoClienteListResponse(Pedido pedido) {
+		super();
+		this.idPedido = pedido.getIdPedido();
+		this.sabor = pedido.getSabor();
+		this.quantidade = pedido.getQuantidade();
+	}
+	
 }
